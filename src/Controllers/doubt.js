ca54dbebf5980e706304fb53eff56e4816f340a1
roopesh04 <arvapalli12345@gmail.com>
2021-04-29 10:52:47 +0530

    Completed doubt route

--- src/Controllers/doubt.js
+++ src/Controllers/doubt.js
@@ -20,6 +20,39 @@ const upload_doubt=async(req,res)=>{
     res.status(200).send(doubt)
 }
 
+const get_doubt=async(req,res)=>{
+    const doubt =await Doubt.findById(req.params.id)
+    if(!doubt) throw new Error("They are no doubts under this id")
+    res.status(200).send(doubt)
+}
+
+const get_doubts=async(req,res)=>{
+    let student=await Student.findById(req.user._id)
+    if(!student) throw new Error("There is no user based on this ID")
+
+    const doubts=await Doubt.find({STID:req.user._id})
+    res.status(200).send(doubts)
+}
+
+const update_doubt=async(req,res)=>{
+    const {error}=validateUpdate(req.body)
+    if(error) throw new Error(error.details[0].message)
+
+    let student=await Student.findById(req.user._id)
+    if(!student) throw new Error("There is no user based on this ID")
+    
+    let doubt=await Doubt.findOne({STID:req.user._id,_id:req.params.id})
+    if(!doubt) throw new Error("There is no doubts based on this ID or you are not allowed to update this")
+    
+    handleUpdate(doubt,req.body)
+    doubt=await doubt.save()
+    
+    res.status(200).send(doubt)
+}
+
 module.exports={
-    upload_doubt
+    upload_doubt,
+    update_doubt,
+    get_doubts,
+    get_doubt
 }
--- src/Routes/doubt.js
+++ src/Routes/doubt.js
@@ -1,5 +1,8 @@
 const {
-    upload_doubt
+    upload_doubt,
+    update_doubt,
+    get_doubts,
+    get_doubt
 }=require("../Controllers/doubt")
 
 const auth=require("../Middlewares/auth")
@@ -8,5 +11,11 @@ const express=require("express")
 const router = express.Router();
 
 router.post("/",auth,async(req, res)=>await upload_doubt(req,res))
+
+router.get("/:id",auth,async(req,res)=>await get_doubt(req,res))
+
+router.get("/",auth,async(req,res)=>await get_doubts(req,res))
+
+router.put("/:id",auth,async(req,res)=>await update_doubt(req,res))
 
 module.exports = router
