1654d81ccc98c5b36da711bf772c8ba60579f810
roopesh04 <arvapalli12345@gmail.com>
2021-05-01 16:51:58 +0530

    Updated doubt route

--- src/Routes/doubt.js
+++ src/Routes/doubt.js
@@ -2,20 +2,69 @@ const {
     upload_doubt,
     update_doubt,
     get_doubts,
-    get_doubt
+    get_doubt,
+    add_comment,
+    delete_comment
 }=require("../Controllers/doubt")
 
 const auth=require("../Middlewares/auth")
 const express=require("express")
+const multer=require("multer")
 
 const router = express.Router();
+let upload = multer({ dest: "uploads/"});
 
-router.post("/",auth,async(req, res)=>await upload_doubt(req,res))
+function findnames(list){
+    newList=[]
+    list.forEach(val=>{
+        newList.push(val.filename)
+    })
+    return newList
+}
 
-router.get("/:id",auth,async(req,res)=>await get_doubt(req,res))
+router.post("/",auth,
+    upload.array('attachments'),
+    async(req,res)=>{
+        req.body.attachments=findnames(req.files)
+        await upload_doubt(req,res)
+    }
+)
 
-router.get("/",auth,async(req,res)=>await get_doubts(req,res))
+router.get("/:id",async(req,res)=>await get_doubt(req,res))
 
-router.put("/:id",auth,async(req,res)=>await update_doubt(req,res))
+router.get("/",async(req,res)=>await get_doubts(req,res))
+
+router.put(
+    "/:id",auth,
+    upload.array('attachments'),
+    async(req,res)=>{
+        if(req.files.length!==undefined){
+            if(req.files.length!==0){
+                req.body.attachments=findnames(req.files)
+            }
+        }
+        await update_doubt(req,res)
+    }
+)
+
+router.post(
+    "/comment/:id",auth,
+    upload.array('attachments'),
+    async(req,res)=>{
+        if(req.files.length!==undefined){
+            if(req.files.length!==0){
+                req.body.attachments=findnames(req.files)
+            }
+        }
+        await add_comment(req,res)
+    }
+)
+
+router.delete(
+    "/:id&:ide",auth,
+    async(req,res)=>{
+        await delete_comment(req,res)
+    }
+)
 
 module.exports = router
