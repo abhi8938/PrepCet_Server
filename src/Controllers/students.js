c7dae58315a9f8a090087e51c2e1507dddddf3f4
Abhishek Singh <abhishek8938@gamil.com>
2021-05-29 15:34:03 +0530

    final 29 may 3:34

--- src/Controllers/students.js
+++ src/Controllers/students.js
@@ -152,7 +152,6 @@ const change_password = async (req, res) => {
 };
 
 const authenticate = async (req, res) => {
-  console.log("reached here", req.body.email);
   if (req.body.signin_method === undefined) {
     const { error } = validateAuth(req.body);
     if (error) throw new Error(error.details[0].message);
@@ -210,44 +209,69 @@ const logoutfromdevice = async (req, res) => {
   res.status(200).send({ message: "You are looged out succefully" });
 };
 
+const creditUpdate = async (id, amount, method) => {
+  let student = await Student.findById(id);
+  if (!student)
+    throw new Error("This is an ivalid token no user in this email id");
 
-const creditUpdate=async(id,amount,method)=>{
-  let student=await Student.findById(id)
-  if(!student) throw new Error("This is an ivalid token no user in this email id")
-
-  if(method=='credit'){
-    let student=await Student.findByIdAndUpdate(id,{$inc:{credits:amount}},{new:true})
-    return student
-  }else if(method=='debit'){
-    let decrease=-1*amount
-    let student=await Student.findByIdAndUpdate(id,{$inc:{wallet:amount/4,credits:decrease}},{new:true})
-    return student
+  if (method == "credit") {
+    let student = await Student.findByIdAndUpdate(
+      id,
+      { $inc: { credits: amount } },
+      { new: true }
+    );
+    return student;
+  } else if (method == "debit") {
+    let decrease = -1 * amount;
+    let student = await Student.findByIdAndUpdate(
+      id,
+      { $inc: { wallet: amount / 4, credits: decrease } },
+      { new: true }
+    );
+    return student;
   }
-}
+};
 
-const apicredits=async(req,res)=>{
-  let student=await creditUpdate(req.user._id,req.body.amount,req.body.method)
-  res.status(200).send(student)
-}
+const apicredits = async (req, res) => {
+  let student = await creditUpdate(
+    req.user._id,
+    req.body.amount,
+    req.body.method
+  );
+  res.status(200).send(student);
+};
 
-const walletUpdate=async(id,amount,method)=>{
-  let student=await Student.findById(id)
-  if(!student) throw new Error("This is an ivalid token no user in this email id")
+const walletUpdate = async (id, amount, method) => {
+  let student = await Student.findById(id);
+  if (!student)
+    throw new Error("This is an ivalid token no user in this email id");
 
-  if(method=='credit'){
-    let student=await Student.findByIdAndUpdate(id,{$inc:{wallet:amount}},{new:true})
-    return student
-  }else if(method=='debit'){
-    let decrease=-1*amount
-    let student=await Student.findByIdAndUpdate(id,{$inc:{wallet:decrease}},{new:true})
-    return student
+  if (method == "credit") {
+    let student = await Student.findByIdAndUpdate(
+      id,
+      { $inc: { wallet: amount } },
+      { new: true }
+    );
+    return student;
+  } else if (method == "debit") {
+    let decrease = -1 * amount;
+    let student = await Student.findByIdAndUpdate(
+      id,
+      { $inc: { wallet: decrease } },
+      { new: true }
+    );
+    return student;
   }
-}
+};
 
-const apiwallet=async(req,res)=>{
-  let student=await walletUpdate(req.user._id,req.body.amount,req.body.method)
-  res.status(200).send(student)
-}
+const apiwallet = async (req, res) => {
+  let student = await walletUpdate(
+    req.user._id,
+    req.body.amount,
+    req.body.method
+  );
+  res.status(200).send(student);
+};
 
 module.exports = {
   logoutfromdevice,
@@ -262,5 +286,5 @@ module.exports = {
   apicredits,
   creditUpdate,
   apiwallet,
-  walletUpdate
-}
+  walletUpdate,
+};
